ab1dca98a02a2f2696b4ab21a9a263228779b995
Prabhat <10p14ed0078@gmail.com>
2026-01-12 14:59:02 +0530

    done

--- app/page.js
+++ app/page.js
@@ -471,6 +471,8 @@ export default function Home() {
                           alt={image.prompt}
                           className="w-full h-full object-cover"
                           loading="lazy"
+                          crossOrigin="anonymous"
+                          data-image-index={image.index}
                         />
                       )}
                     </div>
